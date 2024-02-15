1ddf805727a95f05ddd7bf55697fa672e6a55076
sundevista <ka1tzyu@gmail.com>
2024-02-15 16:05:04 +0200

    fix: support for all types of attachment folder setting

--- src/apis/youtube.ts
+++ src/apis/youtube.ts
@@ -1,102 +1,100 @@
 import { App, TFolder, requestUrl } from 'obsidian';
-import { filterStringData, parseAttachmentFolder, parseChapters, parseISODuration, parseVideoId } from 'src/utils/parser';
+import { filterStringData, getAttachmentFolder, parseChapters, parseISODuration, parseVideoId } from 'src/utils/parser';
 import { YouTubeTemplatePluginSettings } from '../settings';
 import { VideoData } from '../types/video-data';
 import { ChannelListResponse, Thumbnail, VideoListResponse } from '../types/youtube-response';
 import {
-	NO_CHANNEL_ERROR,
-	NO_INTERNET_CATCHING_ERROR,
-	NO_INTERNET_ERROR,
-	NO_VIDEO_ERROR,
-	WRONG_API_KEY_ERROR,
+  NO_CHANNEL_ERROR,
+  NO_INTERNET_CATCHING_ERROR,
+  NO_INTERNET_ERROR,
+  NO_VIDEO_ERROR,
+  WRONG_API_KEY_ERROR,
 } from '../utils/constants';
 
 const baseUrlForVideos = 'https://www.googleapis.com/youtube/v3/videos?';
 const baseUrlForChannels = 'https://www.googleapis.com/youtube/v3/channels?';
 
 export async function getVideoData(
-	videoUrl: string,
-	settings: YouTubeTemplatePluginSettings,
-	downloadThumbnail: boolean,
+  videoUrl: string,
+  settings: YouTubeTemplatePluginSettings,
+  downloadThumbnail: boolean,
 ): Promise<VideoData> {
-	let thumbnailLink = '';
+  let thumbnailLink = '';
 
-	try {
-		const videoResponse: VideoListResponse = await requestUrl(
-			baseUrlForVideos + `part=snippet,contentDetails&id=${parseVideoId(videoUrl)}&key=${settings.googleCloudApiKey}`,
-		).json;
+  try {
+    const videoResponse: VideoListResponse = await requestUrl(
+      baseUrlForVideos + `part=snippet,contentDetails&id=${parseVideoId(videoUrl)}&key=${settings.googleCloudApiKey}`,
+    ).json;
 
-		if (videoResponse.items.length === 0) {
-			throw new Error(NO_VIDEO_ERROR);
-		}
+    if (videoResponse.items.length === 0) {
+      throw new Error(NO_VIDEO_ERROR);
+    }
 
-		const channelsResponse: ChannelListResponse = await requestUrl(
-			baseUrlForChannels +
-			`part=statistics&id=${videoResponse.items[0].snippet.channelId}&key=${settings.googleCloudApiKey}`,
-		).json;
+    const channelsResponse: ChannelListResponse = await requestUrl(
+      baseUrlForChannels + `part=statistics&id=${videoResponse.items[0].snippet.channelId}&key=${settings.googleCloudApiKey}`,
+    ).json;
 
-		if (channelsResponse.items.length === 0) {
-			throw new Error(NO_CHANNEL_ERROR);
-		}
+    if (channelsResponse.items.length === 0) {
+      throw new Error(NO_CHANNEL_ERROR);
+    }
+    if (downloadThumbnail) {
+      const thumbnails = videoResponse.items[0].snippet.thumbnails;
+      thumbnailLink = (await downloadVideoThumbnail(this.app, Object.values(videoResponse.items[0].snippet.thumbnails))) ?? '';
+    }
 
-		console.log(videoResponse);
-		console.log(channelsResponse);
+    return {
+      id: videoResponse.items[0].id,
+      title: filterStringData(videoResponse.items[0].snippet.title),
+      channelName: filterStringData(videoResponse.items[0].snippet.channelTitle),
+      subscribers: parseInt(channelsResponse.items[0].statistics.subscriberCount),
+      length: parseISODuration(videoResponse.items[0].contentDetails.duration),
+      //@ts-ignore
+      publishDate: moment(videoResponse.items[0].snippet.publishedAt).format('YYYY-MM-DD'),
+      thumbnail: thumbnailLink ?? '',
+      chapters: parseChapters(videoResponse.items[0].snippet.description).map(
+        (chapter) => `${chapter.timestamp} ${filterStringData(chapter.title)}`,
+      ),
+      hashtags: videoResponse.items[0].snippet.tags ?? [],
+      description: '',
+      //@ts-ignore
+      noteCreated: moment().format('YYYY-MM-DD, HH:mm'),
+      youtubeUrl: videoUrl,
+    };
+  } catch (error) {
+    if (error?.message === NO_INTERNET_CATCHING_ERROR) {
+      throw new Error(NO_INTERNET_ERROR);
+    } else if (error?.status && error.status.toString().startsWith('4')) {
+      throw new Error(WRONG_API_KEY_ERROR);
+    }
 
-		if (downloadThumbnail) {
-			const thumbnails = videoResponse.items[0].snippet.thumbnails;
-			thumbnailLink = (await downloadVideoThumbnail(this.app, Object.values(videoResponse.items[0].snippet.thumbnails))) ?? '';
-		}
-
-		return {
-			id: videoResponse.items[0].id,
-			title: filterStringData(videoResponse.items[0].snippet.title),
-			channelName: filterStringData(videoResponse.items[0].snippet.channelTitle),
-			subscribers: parseInt(channelsResponse.items[0].statistics.subscriberCount),
-			length: parseISODuration(videoResponse.items[0].contentDetails.duration),
-			//@ts-ignore
-			publishDate: moment(videoResponse.items[0].snippet.publishedAt).format('YYYY-MM-DD'),
-			thumbnail: thumbnailLink ?? '',
-			chapters: parseChapters(videoResponse.items[0].snippet.description).map(
-				(chapter) => `${chapter.timestamp} ${filterStringData(chapter.title)}`,
-			),
-			hashtags: videoResponse.items[0].snippet.tags ?? [],
-			description: '',
-			//@ts-ignore
-			noteCreated: moment().format('YYYY-MM-DD, HH:mm'),
-			youtubeUrl: videoUrl,
-		};
-	} catch (error) {
-		if (error?.message === NO_INTERNET_CATCHING_ERROR) {
-			throw new Error(NO_INTERNET_ERROR);
-		} else if (error?.status && error.status.toString().startsWith('4')) {
-			throw new Error(WRONG_API_KEY_ERROR);
-		}
-
-		throw Error(error?.message ? error.message : error);
-	}
+    throw Error(error?.message ? error.message : error);
+  }
 }
 
 export async function downloadVideoThumbnail(app: App, availableThumbnaisl: Thumbnail[]): Promise<string | undefined> {
-	let bestThumbnailIdx = 0;
+  let bestThumbnailIdx = 0;
 
-	for (let i = 1; i < availableThumbnaisl.length; i++) {
-		if (availableThumbnaisl[i].width > availableThumbnaisl[bestThumbnailIdx].width) {
-			bestThumbnailIdx = i;
-		}
-	}
+  for (let i = 1; i < availableThumbnaisl.length; i++) {
+    if (availableThumbnaisl[i].width > availableThumbnaisl[bestThumbnailIdx].width) {
+      bestThumbnailIdx = i;
+    }
+  }
 
-	const imageUrl = availableThumbnaisl[bestThumbnailIdx].url;
+  const imageUrl = availableThumbnaisl[bestThumbnailIdx].url;
 
-	const response = await requestUrl(imageUrl);
+  const response = await requestUrl(imageUrl);
 
-	const filename = `${new Date().getTime()}.${imageUrl.split('.').pop()}`;
-	const abstractFile = this.app.vault.getAbstractFileByPath(parseAttachmentFolder(app.vault.getConfig('attachmentFolderPath')));
+  const filename = `${new Date().getTime()}.${imageUrl.split('.').pop()}`;
+  const attachmentFolderPath = getAttachmentFolder(app);
+  const abstractFile = this.app.vault.getAbstractFileByPath(attachmentFolderPath);
 
-	if (!(abstractFile instanceof TFolder)) {
-		throw new Error(`Attachment folder '${app.vault.getConfig('attachmentFolderPath')}' does not exist. Check if the folder path is correct in your Settings → Files and links → Default location for new attachments.`);
-	}
+  if (!(abstractFile instanceof TFolder)) {
+    throw new Error(
+      `Attachment folder '${attachmentFolderPath}' does not exist. Check if the folder path is correct in your Settings → Files and links → Default location for new attachments.`,
+    );
+  }
 
-	await app.vault.createBinary(`${app.vault.getConfig('attachmentFolderPath')}/${filename}`, response.arrayBuffer);
+  await app.vault.createBinary(`${app.vault.getConfig('attachmentFolderPath')}/${filename}`, response.arrayBuffer);
 
-	return filename;
+  return filename;
 }
--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,62 +1,67 @@
-import { moment } from 'obsidian';
+import { App, moment } from 'obsidian';
 
 export function parseVideoId(url: string): string | null {
-	const regex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
-	const result = regex.exec(url);
-	return result ? result[3] : null;
+  const regex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
+  const result = regex.exec(url);
+  return result ? result[3] : null;
 }
 
 export function parseChapters(description: string): {
-	timestamp: string;
-	title: string;
+  timestamp: string;
+  title: string;
 }[] {
-	// Extract timestamps (either 00:00:00, 0:00:00, 00:00 or 0:00)
-	const lines = description.split('\n');
-	const regex = /(\d{0,2}:?\d{1,2}:\d{2})/g;
-	const chapters = [];
+  // Extract timestamps (either 00:00:00, 0:00:00, 00:00 or 0:00)
+  const lines = description.split('\n');
+  const regex = /(\d{0,2}:?\d{1,2}:\d{2})/g;
+  const chapters = [];
 
-	for (const line of lines) {
-		// Match the regex and check if the line contains a matched regex
-		const matches = line.match(regex);
-		if (matches) {
-			const ts = matches[0];
-			const title = line
-				.split(' ')
-				.filter((l) => !l.includes(ts))
-				.join(' ');
+  for (const line of lines) {
+    // Match the regex and check if the line contains a matched regex
+    const matches = line.match(regex);
+    if (matches) {
+      const ts = matches[0];
+      const title = line
+        .split(' ')
+        .filter((l) => !l.includes(ts))
+        .join(' ');
 
-			chapters.push({
-				timestamp: ts,
-				title: title,
-			});
-		}
-	}
+      chapters.push({
+        timestamp: ts,
+        title: title,
+      });
+    }
+  }
 
-	return chapters;
+  return chapters;
 }
 
 export function parseISODuration(data: string): string {
-	const duration = moment.duration(data);
-	return `${duration.hours() > 0 ? duration.hours() + ':' : ''}${duration.minutes()}:${duration.seconds()}`;
+  const duration = moment.duration(data);
+  return `${duration.hours() > 0 ? duration.hours() + ':' : ''}${duration.minutes()}:${duration.seconds()}`;
 }
 
 export function filterFilename(text: string): string {
-	return text.replace(/[/\\?%*:|"<>]/g, '');
+  return text.replace(/[/\\?%*:|"<>]/g, '');
 }
 
 export function filterStringData(text: string): string {
-	return text.replace(/"(.*?)"/g, '«$1»').replace(/["]/g, '');
+  return text.replace(/"(.*?)"/g, '«$1»').replace(/["]/g, '');
 }
 
+export function getAttachmentFolder(app: App): string {
+  const attachentPath = app.vault.getConfig('attachmentFolderPath');
 
-/**
- * Parses the attachment folder path. If folder starts with a dot or is empty, it will be replaced with a root folder.
- * 
- * @param folder - The folder path to parse.
- * @returns The parsed folder path.
- */
-export function parseAttachmentFolder(folder: string): string {
-	folder = folder.trim();
-	folder = folder.startsWith('.') ? folder.replace('.', '') : folder;
-	return folder === '' ? '/' : folder;
+  if (attachentPath.startsWith('./')) {
+    if (attachentPath.length === 2) return '/';
+
+    const activeFile = app.workspace.getActiveFile();
+    if (!activeFile) throw new Error("No active file, can't parse attachment folder");
+
+    let parentFolder = activeFile?.parent?.path;
+    if (parentFolder?.startsWith('/')) parentFolder = parentFolder.substring(1);
+
+    return parentFolder + attachentPath.substring(2);
+  }
+
+  return attachentPath;
 }
